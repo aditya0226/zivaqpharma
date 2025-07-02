7e8a8a55591d8dcf731a6da802581157b7404de9
sharma.aditya0226 <sharma.aditya0226@gmail.com>
2025-07-02 18:26:51 +0530

    added githubpages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     env: {
         NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
       },
-    
+      output: 'export',
+  basePath: isGithubPages ? '' : '',
+  assetPrefix: isGithubPages ? '/' : '',
 };
 
 export default nextConfig;
